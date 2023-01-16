14d45de775b7a35c7fbabc9a2386ff64336bc717
Diego Bastidas <diego.bastidas@bitsamericas.com>
2023-01-16 09:11:32 -0500

    first commit

---
+++ makefile
@@ -0,0 +1,37 @@
+SERVER_DIR=/usr/share/nginx/html
+
+vagrant.start: vagrant.init execute.composer config.database create.database import.database
+	cd ./virtualizacion/vagrant && vagrant ssh
+vagrant.init:
+	vagrant up
+execute.composer:
+	@echo "======DELETE COMPOSER.LOCK==========="
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd $(SERVER_DIR) && rm -rf composer.lock"
+	@echo "======CLEAR COMPOSER CACHE:==========="
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd && composer clear-cache"
+	@echo "========  COMPOSER AUTH  ============="
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd && composer config -a -g http-basic.gitlab.tigocloud.net '$(username)' '$(pass)'"
+	@echo "=========COMPOSER INSTALL============="
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd $(SERVER_DIR) && composer install -n --prefer-source"
+	@echo "=========COMPOSER UPDATE============="
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd $(SERVER_DIR) && composer update bits/* -n "
+	# @echo $(var)
+config.database:
+	vagrant ssh -- "cd && sudo systemctl stop mysqld"
+	@echo "=================================="
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd && sudo systemctl set-environment MYSQLD_OPTS="--skip-grant-tables""
+	@echo "=================================="
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd && sudo systemctl start mysqld"
+	@echo "=================================="
+create.database:
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd && mysql -uroot -e 'create database $(database_name)'"
+import.database:
+	@echo "=== IMPORTANDO  $(database_name) POR FAVOR, ESPERE ===="
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd && cd $(SERVER_DIR)/virtualizacion/vagrant/database && mysql -u root $(database_name) < db.mysql"
+	@echo "=== base de datos $(database_name) Importada correctamente! ===="
+render.assets:
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd && drush -y config-set system.performance css.preprocess 0"
+	cd ./virtualizacion/vagrant && vagrant ssh -- "cd && drush -y config-set system.performance js.preprocess 0"
+
+
+
---
+++ vagrant/box_files/dependencias.sh
@@ -0,0 +1,55 @@
+sudo su
+
+# INSTALL AND CONFIGURE NGINX
+cp /usr/share/nginx/html/virtualizacion/vagrant/box_files/nginx.repo /etc/yum.repos.d/
+yum -y update && yum -y install nginx && yum -y install initscripts
+cp /usr/share/nginx/html/virtualizacion/vagrant/box_files/default.conf /etc/nginx/conf.d/
+
+# INSTALL AND CONFIGURE PHP
+yum install gcc-c++ zlib-devel amazon-linux-extras -y
+amazon-linux-extras enable php7.4 && amazon-linux-extras install php7.4 -y
+yum -y install php php-cli php-json php-xml php-opcache php-fpm php-intl php-mbstring gd gd-devel php-gd php-zip wget unzip
+systemctl start nginx && systemctl enable nginx.service && systemctl enable php-fpm.service
+
+# INSTALL AND CONFIGURE GIT
+yum -y install git
+
+# INSTALL AND CONFIGURE COMPOSER
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"   \
+&& php composer-setup.php --install-dir=/usr/local/bin --filename=composer  \
+&& php -r "unlink('composer-setup.php');"   \
+&& ln -s /usr/local/bin/composer /usr/local/bin/composer.phar   \
+&& composer self-update --1 \
+&& composer --version \
+&& composer
+
+# INSTALL AND CONFIGURE DRUSH
+curl -sS https://getcomposer.org/installer | php\
+&& mv composer.phar /usr/local/bin/composer \
+&& ln -s /usr/local/bin/composer /usr/bin/composer  \
+&& git clone https://github.com/drush-ops/drush.git /usr/local/src/drush \
+&& cd /usr/local/src/drush && git checkout 9.x  \
+&& ln -s /usr/local/src/drush/drush /usr/bin/drush
+cd /usr/local/src/drush && composer install
+
+# Install and Configure Drupal Console -> https://github.com/hechoendrupal/drupal-console-launcher
+cd /tmp && curl https://github.com/hechoendrupal/drupal-console-launcher/releases/download/1.9.7/drupal.phar -L -o drupal.phar  \
+&& mv drupal.phar /usr/local/bin/drupal \
+&& chmod +x /usr/local/bin/drupal
+
+#INSTALAR MYSQL -> https://tecadmin.net/how-to-install-mysql-8-on-amazon-linux-2/
+yum -y install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
+amazon-linux-extras install epel -y
+yum -y install mysql-community-server
+systemctl enable --now mysqld
+# FIN INSTALAR MYSQL
+
+# RM Nginx default files:
+rm /usr/share/nginx/html/50x.html /usr/share/nginx/html/index.html
+
+# Config Folders
+mkdir /usr/share/nginx/html/web/sites/default/files
+mkdir /usr/share/nginx/html/web/sites/default/files/translations
+cd  /usr/share/nginx/html/web/sites/default/ && chmod -R 777 files
+
+# https://stackoverflow.com/questions/60047819/drupal-installation-default-page-not-loading-css
